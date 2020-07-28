e95cd6ddfaf606c475d492da39a69f49a6318291
carcinogenDEV <53064083+carcinogenDEV@users.noreply.github.com>
2020-07-28 15:36:15 -0600

    committed lmao

--- eventActions/messageFilter.js
+++ eventActions/messageFilter.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const { off } = require('process');
 let warningNumber = 5 //change to client input
 let warningMax;
-let filterToggle = false
+let filterToggle = false;
 let perms = message.member.permissions;
 const cont = message.content.toLowerCase();
 
@@ -13,10 +13,10 @@ if (cont.includes(`!toggle filter` && (message.author.perms.has("ADMINISTRATOR")
         message.channel.send('which words would you like to ban? (be sure to evenly space the banned words in one message)').then
         if (cont.includes('')){
             const bannedWords = cont;
-            const bannedArray = bannedwords.split(' ');
-        }
-        if (message.content.includes(""))
-        filterToggle === true 
+            const bannedArray = bannedwords.split(' '); //makes the input message an array which we will use for the filter
+        } then
+
+        filterToggle === true;
         message.channel.send({embed: {
             color: ('#5E81AC'),
             title: ":triangular_flag_on_post:  Filter toggle :triangular_flag_on_post: ",
@@ -26,36 +26,28 @@ if (cont.includes(`!toggle filter` && (message.author.perms.has("ADMINISTRATOR")
         message.channel.send({embed: {
             color: ('#5E81AC'),
             title: ':triangular_flag_on_post: Filter toggle :triangular_flag_on_post: ',
-            description: "Profanity filter disabled"}})
-    }
-    }
-}
+            description: "Profanity filter disabled"}});
+    };
+    };
+};
 
 
 
 //actual filter code
 class messageFilter {
     static async checkForProfanity(client, message) {
-        const bannedWords = ["bitch", "cock", "dick", "fag", "fuck", "idiot", "piss", "pussy", "porn", "retard", "shit", "tranny"];
         const lowerCase = message.content.toLowerCase();
         // grab all profanity
-        const containedProfanity = bannedWords.some(substring => {
+        const containedProfanity = bannedArray.some(substring => {
             return lowerCase.includes(substring);
     })
 
         if (containedProfanity && (filterToggle = true)) {
-            const warnedUser = message.guild.member(user);
+            const warnedUser = message.channel.author(user);
             warningNumber++;
-            // const embedMessage = new Discord.RichEmbed()
-            //     .setColor('#ffffff')
-            //     .setTitle('Oopsy someone said a fucky wucky word')
-            //     .setDescription(`Someone said fuck in ${message.channel}`)
-            //     .addField('User', message.author)
-            //     .addField('Content', message.content)
-            // message.reply(embedMessage);
             message.channel.send({embed: {
                 color: ('#5E81AC'),
-                description: "profanity caught :sunglasses:"}});
+                description: `profanity caught :sunglasses: warning ${warningNumber}/${warningMax}`}});
                 if (warningNumber => warningMax){
                     warnedUser.ban({reason: 'You were kicked for saying the fucky wucky word'})
                         message.channel.send(`Kicked ${warnedUser.tag} :sunglasses:`);
@@ -65,7 +57,7 @@ class messageFilter {
         
                 }
             }};              
-        
+
         console.log(message.content)
     
 
